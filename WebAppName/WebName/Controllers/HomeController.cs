1780b257a3a4187c2c18218176e9bbe8d6e95064
VadimBondar <ipz212_bvm@student.ztu.edu.ua>
2023-03-27 00:44:26 +0200

    lab01

---
+++ WebAppName/WebName/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebName.Models;
+
+namespace WebName.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult NameInput()
+        {
+            return View();
+        }
+        public IActionResult Name(string name)
+        {
+            ViewData["UserName"] = name;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
